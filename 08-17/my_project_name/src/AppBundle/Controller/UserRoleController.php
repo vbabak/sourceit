d36e793c0e0bc7e9c1a63398862f6ea0b3084105
Vladyslav Babak <marmalade.vlad@gmail.com>
2017-10-22 14:38:04 +0300

    my_project_name

---
+++ 08-17/my_project_name/src/AppBundle/Controller/UserRoleController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\UserRole;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserRoleController extends Controller
+{
+    /**
+     * @Route("/user-roles", name="user_roles_page")
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user_roles_repository = $em->getRepository(UserRole::class);
+        $roles = $user_roles_repository->findBy([], 'id', 0, 10);
+
+        var_dump($roles);
+        exit;
+
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+}
